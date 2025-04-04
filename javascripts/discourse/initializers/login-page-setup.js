9ed187aa8fa5b090c4c9a8521710d8316a85e66d
Konstantin Rafalskii <kraft001@gmail.com>
2025-04-04 10:22:39 +0200

    Remove duplicate files

--- javascripts/discourse/initializers/login-page-setup.js
+++ javascripts/discourse/initializers/login-page-setup.js
@@ -22,42 +22,6 @@ export default {
             link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css';
             document.head.appendChild(link);
           }
-          
-          // Make sure the accordion functionality works
-          setTimeout(() => {
-            const faqItems = document.querySelectorAll('.faq-item');
-            
-            faqItems.forEach(item => {
-              const question = item.querySelector('.faq-question');
-              const answer = item.querySelector('.faq-answer');
-              
-              if (answer) {
-                answer.style.display = 'none';
-              }
-              
-              if (question) {
-                question.addEventListener('click', function() {
-                  faqItems.forEach(otherItem => {
-                    if (otherItem !== item) {
-                      const otherAnswer = otherItem.querySelector('.faq-answer');
-                      const otherToggle = otherItem.querySelector('.faq-toggle');
-                      if (otherAnswer) otherAnswer.style.display = 'none';
-                      if (otherToggle) otherToggle.innerHTML = '▼';
-                    }
-                  });
-                  
-                  const toggle = item.querySelector('.faq-toggle');
-                  if (answer && answer.style.display === 'none') {
-                    answer.style.display = 'block';
-                    if (toggle) toggle.innerHTML = '▲';
-                  } else if (answer) {
-                    answer.style.display = 'none';
-                    if (toggle) toggle.innerHTML = '▼';
-                  }
-                });
-              }
-            });
-          }, 1000);
         } else {
           // Remove classes when not on login page
           document.body.classList.remove('login-required');
